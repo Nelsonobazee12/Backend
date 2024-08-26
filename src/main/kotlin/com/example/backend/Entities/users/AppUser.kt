fc5a5dad8d839aea80cf7f8288c80a968396cc52
Nelson <nelsonobazee12@gmail.com>
2024-08-26 11:30:58 +0100

    added new features and update

--- src/main/kotlin/com/example/backend/Entities/users/AppUser.kt
+++ src/main/kotlin/com/example/backend/Entities/users/AppUser.kt
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 @Table(name = "app_user")
-class AppUser(
+data class AppUser(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
