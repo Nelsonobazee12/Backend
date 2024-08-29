880171f15c1241bd3f22386b100ceba6920c53ad
Nelsonobazee12 <116342964+Nelsonobazee12@users.noreply.github.com>
2024-08-29 15:48:30 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'org.postgresql:postgresql:42.6.0'
+    implementation("org.postgresql:postgresql:42.6.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
     implementation ("org.springframework.boot:spring-boot-starter-validation")
     implementation ("org.springframework.boot:spring-boot-starter-logging")
